fe86c2d4bda8a079ca4f1b61f977e6b7814f59f8
shayan ali <shayanjawed4@gmail.com>
2021-05-09 23:56:39 +0500

    new method

---
+++ forward.py
@@ -0,0 +1,15 @@
+import sympy 
+
+points=[]
+print("Enter the number of value:")
+values=int(input())
+print("1:Values and function values\n2:Values and functions")
+choice=int(input())
+if(choice==1):
+
+else:
+    print("Please enter values\n")
+    for i in range(values):
+        points.append(int(input()))
+    print("Please enter function:")
+    function=input()
