33deffa92229709c43caef949c901cdcc2e3e65c
shayan ali <shayanjawed4@gmail.com>
2021-05-10 15:56:01 +0500

    forward Difference done

--- forward.py
+++ forward.py
@@ -1,19 +1,45 @@
 import sympy as sp
 
-def forward(value,h,func):
-    val=value+h
-    a=sp.N(func.subs(x,val))
-    b=sp.N(func.subs(x,value))
-    c=(a+b)/h
+def forwardDiff(func,index,list,h):
+    a=list[index]+h
+    if a in list:
+        indexs=list.index(a)
+        return (func[indexs]-func[index])/h
+    else:
+        return "Not Possible at this index"
+
+# def backwardDiff(func,index,list,value,h):
+
+def forward(value, h, func):
+    val = value+h
+    a = sp.N(func.subs(x, val))
+    b = sp.N(func.subs(x, value))
+    c = (a-b)/h
     return c
 
-points=[]
+
+points = []
 print("Enter the number of value:")
-values=int(input())
+values = int(input())
 print("1:Values and function values\n2:Values and function")
-choice=int(input())
-if(choice==1):
-    print("w")
+choice = int(input())
+if(choice == 1):
+    forwards = []
+    backward=[]
+    functionVal=[]
+    print("Please enter values and functions\n")
+    for i in range(values):
+        points.append(float(input()))
+        functionVal.append(float(input()))
+    h=points[1]-points[0]
+    hBack=h*(-1)
+    forwardDiff(functionVal,0,points,h)
+    for i in range(len(points)):
+        forwards.append(forwardDiff(functionVal,i,points,h))
+        backward.append(forwardDiff(functionVal,i,points,hBack))
+    for i in range(len(points)):
+        print(forwards[i])
+        print(backward[i])
 else:
     forwards=[]
     backward=[]
